7dcfa42268344548b586bcfbc44ee71686ddd910
Jean Jacques <35910266+FPDPanda@users.noreply.github.com>
2024-05-05 23:31:34 -0300

    Bringing new components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./App.css";
+import Intro from "./components/Intro/Intro";
+import Navigation from "./components/Navigation/Navigation";
+import Projects from "./components/Projects/Projects";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <div></div>;
+  const introSection = useRef(null);
+  const projectsSection = useRef(null);
+  const aboutmeSection = useRef(null);
+  const footerSection = useRef(null);
+
+  return (
+    <div>
+      <Navigation
+        introSectionReference={introSection}
+        projectsSectionReference={projectsSection}
+        aboutmeSectionReference={aboutmeSection}
+        footerSectionReference={footerSection}
+      />
+      <Intro reference={introSection} />
+      <Projects reference={projectsSection} />
+      <AboutMe reference={aboutmeSection} />
+      <Footer reference={footerSection} />
+    </div>
+  );
 }
 
 export default App;
